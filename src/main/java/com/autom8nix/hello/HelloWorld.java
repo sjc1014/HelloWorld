0310185bd281fb0c7dfa53141ef53468fd793c4d
Steve Casazza <sjc1014@gmail.com>
2021-07-29 23:47:12 -0400

    Adding Source Code To Repo

---
+++ src/main/java/com/autom8nix/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.autom8nix.hello;
+
+/**
+ * Hello World Program
+ */
+public class HelloWorld {
+
+  /**
+   * The main method of our class
+   */
+  public static void main(String[] args) {
+
+    System.out.println("Hello World");
+  }
+}
