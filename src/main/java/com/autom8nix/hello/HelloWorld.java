5eb51ce77019bc7b8d2c54b65b5ea1b7fc68f4b1
Steve Casazza <sjc1014@gmail.com>
2021-07-30 01:25:11 -0400

    Updated PM and Main HelloWorld Java Program

--- src/main/java/com/autom8nix/hello/HelloWorld.java
+++ src/main/java/com/autom8nix/hello/HelloWorld.java
@@ -1,15 +1,5 @@
-package com.autom8nix.hello;
-
-/**
- * Hello World Program
- */
 public class HelloWorld {
-
-  /**
-   * The main method of our class
-   */
   public static void main(String[] args) {
-
     System.out.println("Hello World");
   }
 }
