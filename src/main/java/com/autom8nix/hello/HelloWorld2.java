5d2a56fb1cfc459161bc6d91c242c9759d9e52f6
Steve Casazza <sjc1014@gmail.com>
2021-08-03 22:00:41 -0400

    Adding new logic to uncapitalize in HelloWorld2 class

--- src/main/java/com/autom8nix/hello/HelloWorld2.java
+++ src/main/java/com/autom8nix/hello/HelloWorld2.java
@@ -5,5 +5,6 @@ public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(StringUtils.capitalize("hello world"));
+        System.out.println(StringUtils.uncapitalize("HELLO WORLD 2"));
     }
 }
