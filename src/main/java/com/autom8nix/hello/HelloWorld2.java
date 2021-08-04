dea61247e9dec7d63da55fe4d334ce078ffea557
Steve Casazza <sjc1014@gmail.com>
2021-08-03 21:55:16 -0400

    Built project locally using IntelliJ

---
+++ src/main/java/com/autom8nix/hello/HelloWorld2.java
@@ -0,0 +1,9 @@
+package com.autom8nix.hello;
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
